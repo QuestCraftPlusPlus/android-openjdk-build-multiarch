d068304bff0282efd42dc4fed5639bc1e84c1df9
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-06-06 12:39:03 -0700

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --branch "jdk-22+36" --depth 1 https://github.com/openjdk/jdk22u openjdk
+git clone --branch "22.0.1+8-jvmci-b01" --depth 1 https://github.com/graalvm/labs-openjdk-22.git openjdk
