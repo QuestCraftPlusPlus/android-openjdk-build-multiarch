388103fd87e2d87767883f81a401f85a5a849a1b
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-07-07 12:27:46 -0700

    Go back to OpenJDK

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --branch "22.0.1+8-jvmci-b01" --depth 1 https://github.com/graalvm/labs-openjdk-22.git openjdk
+git clone --branch "jdk-22+36" --depth 1 https://github.com/openjdk/jdk.git openjdk
