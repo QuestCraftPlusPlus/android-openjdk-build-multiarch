234a2a132f656220bbfe6b52d7989323ae15e2cf
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2024-10-15 16:53:01 -0500

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -9,8 +9,8 @@ set -e
 # Please change if you have different architecture.
 
 if [ "$BUILD_IOS" != "1" ]; then
-  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-22.0.1_linux-x64_bin.tar.gz
+  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-23.0.1_linux-x64_bin.tar.gz
 else
-  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-22.0.1_macos-aarch64_bin.tar.gz
+  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-23.0.1_macos-aarch64_bin.tar.gz
 fi
 tar xvf openjdk-22*.tar.gz
