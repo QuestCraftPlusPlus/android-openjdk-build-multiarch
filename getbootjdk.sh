ab4744478d0a132695127080c6c0e7e88159e617
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2024-10-15 16:57:56 -0500

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -12,4 +12,5 @@ if [ "$BUILD_IOS" != "1" ]; then
   wget https://download.java.net/java/GA/jdk23.0.1/c28985cbf10d4e648e4004050f8781aa/11/GPL/openjdk-23.0.1_linux-x64_bin.tar.gz
 else
   wget https://download.java.net/java/GA/jdk23.0.1/c28985cbf10d4e648e4004050f8781aa/11/GPL/openjdk-23.0.1_macos-aarch64_bin.tar.gz
+fi
 tar xvf openjdk-22*.tar.gz
