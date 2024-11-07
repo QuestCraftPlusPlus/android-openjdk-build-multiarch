bdb04bc45b12c97e58b42ed5e454b9e4d93332aa
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2024-11-06 20:59:13 -0600

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
-git clone --branch "jdk-22+36" --depth 1 https://github.com/openjdk/jdk.git openjdk
+git clone https://github.com/openjdk/jdk.git openjdk
+cd openjdk
+git checkout pull/20677
