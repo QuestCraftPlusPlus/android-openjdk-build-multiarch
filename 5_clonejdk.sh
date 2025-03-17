bef032e766dcfb6a7c8cb9e18ec1a6b4c3b42b0a
The Judge <53906078+thejudge156@users.noreply.github.com>
2025-03-16 19:05:25 -0700

    Update 5_clonejdk.sh

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ $TARGET_VERSION -eq 24 ]]; then
-    git clone --branch "JDK-8305895-v4" --depth 1 https://github.com/rkennke/jdk.git openjdk
+    git clone --branch pull/20677 --depth 1 https://github.com/openjdk/jdk.git openjdk
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
 fi
